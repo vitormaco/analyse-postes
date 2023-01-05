80f13450ea6f80d3d59f19311a07bdaefd51ffcf
Vitor Correa <vitor.correa@localpayment.com>
2023-01-05 19:04:41 +0100

    fixed original human error

--- analysis.r
+++ analysis.r
@@ -6,44 +6,46 @@ library("plyr")
 library("dplyr")
 library("magrittr")
 library("cowplot")
+library("geojsonio")
+library("broom")
 
 postes <- read.csv("./dataset/postes_2020.csv", sep = ";")
 mapping <- read.csv("./dataset/varmod_postes_2020.csv", sep = ";")
 
-# Setup carte
+# Read geojson
+spdf <- geojson_read('departements.geojson',what='sp')
+spdf_fortified <- tidy(spdf,region="code")
 
-spdf <- geojson_read("departements.geojson", what = "sp")
-spdf_fortified <- tidy(spdf, region = "code")
+# Percentage of people earing more/less than X euros yearly per region
+trancheRevenuParRegion <- postes %>% select(TRBRUTT,DEPR) %>% filter(DEPR != "") %>% dplyr::count(DEPR,TRBRUTT)
+nbPeoplePerRegion <- trancheRevenuParRegion %>% group_by(DEPR) %>% dplyr::summarise(n = sum(n))
 
-# Percentage of people earningh more/less than X euros yearly per region
-tranchesRevenuParRegion <- postes %>%
-    select(TRBRUTT, DEPR) %>%
-    filter(DEPR != "") %>%
-    count(DEPR, TRBRUTT)
-percentageRevenuParRegion <- tranchesRevenuParRegion %>%
-    group_by(DEPR) %>%
-    mutate(percentage = n / sum(n)) %>%
-    filter(TRBRUTT >= 22) %>%
-    group_by(DEPR) %>%
-    summarise(highPercentage = sum(percentage))
-percentageRevenuParRegion["lowPercentage"] <- tranchesRevenuParRegion %>%
-    group_by(DEPR) %>%
-    mutate(percentage = n / sum(n)) %>%
-    filter(TRBRUTT <= 13) %>%
-    group_by(DEPR) %>%
-    summarise(lowPercentage = sum(percentage)) %>%
-    select(lowPercentage)
-percentageRevenuParRegion$DEPR <- as.character(percentageRevenuParRegion$DEPR)
+tmp <- trancheRevenuParRegion %>% filter(TRBRUTT <= 12)
+tmp <- tmp %>% group_by(DEPR) %>% dplyr::summarise(regroup = sum(n))
+tmp <- tmp %>% left_join(. , nbPeoplePerRegion, by=c("DEPR"="DEPR"))
+percentageRevenuParRegion$lowPercentage <- tmp$regroup/tmp$n
+
+
+tmp <- trancheRevenuParRegion %>% filter(TRBRUTT > 21)
+tmp <- tmp %>% group_by(DEPR) %>% dplyr::summarise(regroup = sum(n))
+tmp <- tmp %>% left_join(. , nbPeoplePerRegion, by=c("DEPR"="DEPR"))
+percentageRevenuParRegion$highPercentage <- tmp$regroup/tmp$n
+
+percentageRevenuParRegion$DEPR = as.character(percentageRevenuParRegion$DEPR)
+
+
+spdf_fortified <- spdf_fortified %>% left_join(. , percentageRevenuParRegion, by=c("id"="DEPR"))
+
 
 graph1 <- ggplot() +
-    geom_polygon(data = spdf_fortified, aes(fill = lowPercentage, x = long, y = lat, group = group)) +
-    scale_fill_gradient(low = "#eeebc5", high = "#bb0600") +
+    geom_polygon(data = spdf_fortified, aes(fill=lowPercentage,x = long, y = lat, group = group)) +
+    scale_fill_gradient(low='#eeebc5',high='#bb0600') +
     theme_void() +
     coord_map()
 
 graph2 <- ggplot() +
-    geom_polygon(data = spdf_fortified, aes(fill = highPercentage, x = long, y = lat, group = group)) +
-    scale_fill_gradient(low = "#eeebc5", high = "#bb0600") +
+    geom_polygon(data = spdf_fortified, aes(fill=highPercentage,x = long, y = lat, group = group)) +
+    scale_fill_gradient(low='#eeebc5',high='#bb0600') +
     theme_void() +
     coord_map()
 
